954d9cd2513a39c8eb64e649b4004f6f72f31764
billy784512 <billy784512369@gmail.com>
2024-12-29 17:37:57 +0800

    feat: get pdf by blob url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
 import { DocumentAnalysisClient, AzureKeyCredential } from "@azure/ai-form-recognizer";
-import AzureOpenAI from 'openai';
+import { AzureOpenAI } from 'openai';
 import './App.css';
 
 const testMessages = [
@@ -27,11 +27,9 @@ const testMessages = [
 ];
 
 const pdfList = [
-  { title: 'PDF 1', url: '/sample.pdf' },
-  { title: 'PDF 2', url: '/sample.pdf' },
-  { title: 'PDF 3', url: '/sample.pdf' },
-  { title: 'PDF 4', url: '/sample.pdf' },
-  { title: 'PDF 5', url: '/sample.pdf' },
+  { title: 'demo1.pdf', url: 'https://workshoppoc.blob.core.windows.net/pdf/demo1.pdf' },
+  { title: 'demo2.pdf', url: 'https://workshoppoc.blob.core.windows.net/pdf/demo2.pdf' },
+  { title: 'demo3.pdf', url: 'https://workshoppoc.blob.core.windows.net/pdf/demo3.pdf' },
 ];
 
 const endpoint = "https://ai-allen7845123692269ai150842469449.openai.azure.com"
@@ -162,7 +160,7 @@ function App() {
         <ul className="pdf-list">
           {pdfList.map((pdf, index) => (
             <li key={index}><input type="radio" value="pdf2" />
-              <a href="sample.pdf" target="_blank">{pdf.title}</a>
+              <a href={pdf.url} target="_blank">{pdf.title}</a>
             </li>
           ))}
         </ul>
